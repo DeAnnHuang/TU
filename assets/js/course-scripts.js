1e3d827715656ee678b5d9c53e2bdc4f7c6ecad2
deannhuang <deannhuang@github.com>
2017-02-11 16:06:53 +0800

    選課結果postback
    1.新增input#cosid
    2.將複選改單選
    3.將select value 加入input#cosid

--- assets/js/course-scripts.js
+++ assets/js/course-scripts.js
@@ -1,24 +1,37 @@
 var $select = $(".course-class .class");
-var $picked = $("#course-picked"),
-    $picked_value = $(".class.select").length;
-    $picked.html($picked_value);
-    
+// var $picked = $("#course-picked"),
+// $picked_value = $(".class.select").length;
+// $picked.html($picked_value);
+
 $select.each(function() {
 
     $(this).click(function(event) {
-        var $self = $(this);
-        // 排除掉額滿的
-        if (!$self.hasClass('full')) {
+            var $self = $(this);
 
-            // $self.toggleClass('select');
-            if ($self.hasClass('select')) {
-                $self.removeClass('select');
-                $picked_value--;
-            } else {
-                $self.addClass('select');
-                $picked_value++;
+            // 排除掉額滿的
+            if (!$self.hasClass('full')) {
+
+                // $self.toggleClass('select');
+                if ($self.hasClass('select')) {
+                    $self.removeClass('select');
+                    // $picked_value--;
+                } else {
+                    //改成單選
+                    $(".select").removeClass('select');
+
+                    $self.addClass('select');
+
+                    // $picked_value++;
+                }
+                // $picked.html($picked_value);
             }
-            $picked.html($picked_value);
+
+            const $FinalSelection = $(".select"),
+                $input = $('#cosid');
+
+            (!$FinalSelection.get(0)) ?
+            $input.val(''); :
+            $input.val($FinalSelection.attr('value'));
         }
 
     });
